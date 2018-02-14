1d80f19bb98ff18b1e727e326b37147ef498dc9b
Luan Truong <luantruong210@gmail.com>
2018-02-14 13:06:36 -0800

    Initial build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  feature : string
+
+  onNavigate(feature: string) {
+    this.feature = feature;
+  }
 }
---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.css']
+})
+export class RecipesComponent implements OnInit {
+  recipeDetail : Recipe;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onDetail(recipe : Recipe) {
+    console.log("From Recipes: " + recipe);
+    this.recipeDetail = recipe;
+  }
+
+}
